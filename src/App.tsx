4111b94e79e68fe5a137060b975c1f0002c8cde9
BlueGeckoJP <kei09doya@gmail.com>
2025-09-04 01:52:17 +0900

    feat: add filesystem plugin and implement image decoding in WASM

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import { listen } from "@tauri-apps/api/event";
 import { useEffect } from "react";
+import init, { decode_image_to_image_data } from "../src-wasm/pkg/src_wasm";
+import { convertFileSrc } from "@tauri-apps/api/core";
+import { readFile } from "@tauri-apps/plugin-fs";
 
 export default function App() {
+  useEffect(() => {
+    init().then(() => {
+      console.log("WASM module initialized");
+    });
+  }, []);
+
   // Add listener for "open-image" event from Tauri backend
   useEffect(() => {
     let unlisten: (() => void) | undefined;
@@ -11,6 +20,13 @@ export default function App() {
         typeof event.payload === "string"
           ? event.payload
           : String(event.payload);
+      const filePath = convertFileSrc(imagePath);
+      readFile(filePath).then((content) => {
+        const img = decode_image_to_image_data(content);
+        if (img) {
+          console.log("Decoded image data:", img);
+        }
+      });
     }).then((fn) => {
       unlisten = fn;
     });
