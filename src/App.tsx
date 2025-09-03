847501fc477313eabb944b42854ee25887b94547
BlueGeckoJP <kei09doya@gmail.com>
2025-09-04 02:45:47 +0900

    feat: add image canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { listen } from "@tauri-apps/api/event";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import init, { decode_image_to_image_data } from "../src-wasm/pkg/src_wasm";
 import { convertFileSrc } from "@tauri-apps/api/core";
 import { readFile } from "@tauri-apps/plugin-fs";
 
 export default function App() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
   useEffect(() => {
     init().then(() => {
       console.log("WASM module initialized");
@@ -23,9 +25,15 @@ export default function App() {
       const filePath = convertFileSrc(imagePath);
       readFile(filePath).then((content) => {
         const img = decode_image_to_image_data(content);
-        if (img) {
-          console.log("Decoded image data:", img);
-        }
+        if (!img) return;
+
+        const canvas = canvasRef.current;
+        if (!canvas) return;
+
+        const ctx = canvas.getContext("2d");
+        if (!ctx) return;
+
+        ctx.putImageData(img, 0, 0);
       });
     }).then((fn) => {
       unlisten = fn;
@@ -35,5 +43,9 @@ export default function App() {
     };
   }, []);
 
-  return <></>;
+  return (
+    <>
+      <canvas width={800} height={600} ref={canvasRef}></canvas>
+    </>
+  );
 }
